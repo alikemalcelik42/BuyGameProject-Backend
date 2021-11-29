92320376287ec1a0995a73a994956969afda6098
alikemalcelik42 <alikemalcelik42@gmail.com>
2021-11-29 23:09:00 +0300

    29.11.2021

---
+++ Business.Tests/GameManagerTests.cs
@@ -0,0 +1,47 @@
+using Business.Concrete;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+
+namespace Business.Tests
+{
+    [TestClass]
+    public class GameManagerTests
+    {
+        Mock<IGameDal> _mockGameDal;
+        List<Game> _dbGames;
+
+        [TestInitialize]
+        public void Start()
+        {
+            _mockGameDal = new Mock<IGameDal>();
+            _dbGames = new List<Game>(){
+                new Game()
+                {
+                Id = 1,
+                Name = "Mario",
+                UnitPrice = 20
+                },
+                new Game()
+                {
+                Id = 2,
+                Name = "Minecraft",
+                UnitPrice = 50
+                }
+            };
+            _mockGameDal.Setup(g => g.GetAll(null)).Returns(_dbGames); 
+        }
+
+        [TestMethod]
+        public void Must_be_listable()
+        {
+            GameManager gameManager = new GameManager(_mockGameDal.Object);
+            var data = gameManager.GetAll();
+            Assert.IsNotNull(data);
+        }
+    }
+}
--- DataAccess/Abstract/IGameDal.cs
+++ DataAccess/Abstract/IGameDal.cs
@@ -5,6 +5,6 @@ namespace DataAccess.Abstract
 {
     public interface IGameDal : IEntityRepository<Game>
     {
-
+        
     }
 }
